f46fc4c7d93617c62312f080ac65538b570c86ed
plectrum0224 <plectrum@outlook.com>
2016-03-08 18:04:42 +0800

    add standard dialog file

--- PyQt_Study/DialogStudy.py
+++ PyQt_Study/DialogStudy.py
@@ -14,26 +14,26 @@ class PenPropertiesDlg(QDialog):
 
     def __init__(self, parent=None):
         super(PenPropertiesDlg, self).__init__(parent)
-
-        widthLabel = QLabel("&Width")
-        self.widthSpinBox = QSpinBox()
+# =====================界面布局BEGIN==========================================#
+        widthLabel           = QLabel("&Width")
+        self.widthSpinBox    = QSpinBox()
         widthLabel.setBuddy(self.widthSpinBox)
         self.widthSpinBox.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
         self.widthSpinBox.setRange(0, 24)
         self.beveledCheckBox = QCheckBox("&Beveled edges")
-        styleLabel = QLabel("&Style:")
-        self.styleComboBox = QComboBox()
+        styleLabel           = QLabel("&Style:")
+        self.styleComboBox   = QComboBox()
         styleLabel.setBuddy(self.styleComboBox)
         self.styleComboBox.addItems(
-            ["Solid", "Dashed", "Dotted", "DashDotted", "DashDotDotted"])
-        okButton = QPushButton("&OK")
-        cancelButton = QPushButton("Cancel")
+        ["Solid", "Dashed", "Dotted", "DashDotted", "DashDotDotted"])
+        okButton             = QPushButton("&OK")
+        cancelButton         = QPushButton("Cancel")
 
         buttonLayout = QHBoxLayout()
         buttonLayout.addStretch()
         buttonLayout.addWidget(okButton)
         buttonLayout.addWidget(cancelButton)
-        layout = QGridLayout()
+        layout       = QGridLayout()
         layout.addWidget(widthLabel, 0, 0)
         layout.addWidget(self.widthSpinBox, 0, 1)
         layout.addWidget(self.beveledCheckBox, 0, 2)
@@ -41,38 +41,37 @@ class PenPropertiesDlg(QDialog):
         layout.addWidget(self.styleComboBox, 1, 1, 1, 2)
         layout.addLayout(buttonLayout, 2, 0, 1, 3)
         self.setLayout(layout)
-
+# =====================界面布局 END===========================================#
+#==========================连接signal and slot BEGIN=========================#
         self.connect(okButton, SIGNAL("clicked()"),
                      self, SLOT("accept()"))
         self.connect(cancelButton, SIGNAL("clicked()"),
                      self, SLOT("reject()"))
         self.setWindowTitle("Pen PenProperties")
+#==========================连接signal and slot END=========================#
+
 
 class Form(QDialog):
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
-
-        self.width = 10
-        self.beveled = False
-        self.style = "Solid"
-
-        # penButtonInline = QPushButton("Set Pen ....")
-        penButton = QPushButton("Set Pen....Dumb Dialog Panel")
-        self.label = QLabel("The Pen has not been set")
+#创建实例参数
+        self.width        = 10
+        self.beveled      = False
+        self.style        = "Solid"
+#界面布局BEGIN
+        penButton         = QPushButton("Set Pen....Dumb Dialog Panel")
+        self.label        = QLabel("The Pen has not been set")
         self.label.setTextFormat(Qt.RichText)
-
-        layout = QVBoxLayout()
-        # layout.addWidget(penButtonInline)
+        layout            = QVBoxLayout()
         layout.addWidget(penButton)
         layout.addWidget(self.label)
         self.setLayout(layout)
-
-        # self.connect(penButtonInline, SIGNAL("clicked()"),
-        #              self.setPenInline)
-        self.connect(penButton, SIGNAL("clicked()"),
-                     self.setPenProperties)
-
         self.setWindowTitle("Pen")
+#界面布局END
+#连接signal and slot BEGIN
+        self.connect(penButton, SIGNAL("clicked()"),
+        self.setPenProperties)
+#连接signal and slot END
         self.updateData()
 
     def updateData(self):
@@ -81,20 +80,19 @@ class Form(QDialog):
             bevel = "<br>Beveled"
         self.label.setText("Width = %d<br>Style = %s%s" %
                            (self.width, self.style, bevel))
+#slot BEGIN
     def setPenProperties(self):
-        dialog = PenPropertiesDlg(self)
+        dialog       = PenPropertiesDlg(self)
         dialog.widthSpinBox.setValue(self.width)
         dialog.beveledCheckBox.setChecked(self.beveled)
         dialog.styleComboBox.setCurrentIndex(
-                                             dialog.styleComboBox.findText(self.style))
+        dialog.styleComboBox.findText(self.style))
         if dialog.exec_():
-            self.width = dialog.widthSpinBox.value()
+#final value directly from the dialog's widget
+            self.width   = dialog.widthSpinBox.value()
             self.beveled = dialog.beveledCheckBox.isChecked()
-            self.style = str(dialog.styleComboBox.currentText())
+            self.style   = str(dialog.styleComboBox.currentText())
             self.updateData()
-
-
-
 
 
 if __name__ == "__main__":
@@ -102,4 +100,3 @@ if __name__ == "__main__":
     form = Form()
     form.show()
     app.exec_()
-
